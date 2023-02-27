6996df37a9316cf4ac3305032849173460e9855c
anhquan <quan44652@gmail.com>
2023-02-27 14:30:01 +0700

    croll animate, scroll top

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const form = document.querySelector("#form");
 const popup = document.querySelector(".popup");
 const btn_close = document.querySelector(".btn_close");
 const icon_close = document.querySelector(".icon-close");
+const croll_top = document.querySelector("#croll-top");
+const app = document.getElementById("app");
 
 const handlerPopup = (callback) => {
   callback.addEventListener("click", () => {
@@ -19,4 +21,16 @@ form.addEventListener("submit", (e) => {
   setTimeout(() => {
     popup.style.display = "block";
   }, 2000);
+});
+
+window.addEventListener("scroll", (e) => {
+  if (window.scrollY > 1000) {
+    croll_top.style.display = "block";
+  } else {
+    croll_top.style.display = "none";
+  }
+});
+
+croll_top.addEventListener("click", () => {
+  app.scrollIntoView({ behavior: "smooth", block: "start" });
 });
